0d20117f4d6c34f37a5bbdce01189f517e684e68
MUGENSon1KKU <55625230+MUGENSonic@users.noreply.github.com>
2021-07-10 16:02:55 +0800

    Version 2.0.0 alpha 2
    New menu background ; New player controller & weapon swithcing (special thanks to Garbaj for his tutorials); Weapon (do not fire)

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,35 +1,88 @@
 extends KinematicBody
 
-onready var cam = $Camera
+var speed = 7
+const ACCEL_DEFAULT = 7
+const ACCEL_AIR = 1
+onready var accel = ACCEL_DEFAULT
+var gravity = 20
+var jump = 10
 
-var speed = 6
+var cam_accel = 40
+var mouse_sense = 0.5
+var snap
 
 var direction = Vector3()
+var velocity = Vector3()
+var gravityVec = Vector3()
+var movement = Vector3()
 
-remote func _set_pos(position):
-	global_transform.origin = position
+var currentWeapon = 1
+
+onready var head = $Head
+onready var camera = $Head/Camera
+onready var gun1 = $Head/Hand/Gun1
 
 func _ready():
+	#hides the cursor
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		var movement = event.relative
-		cam.rotation.x += -deg2rad(movement.y * GameRun.sensevity)
-		cam.rotation.x = clamp(cam.rotation.x, deg2rad(-90), deg2rad(90))
-		rotation.y += -deg2rad(movement.x * GameRun.sensevity)
+func weapon_select():
+	if Input.is_action_just_pressed("slot1"):
+		currentWeapon = 1
+	elif Input.is_action_just_pressed("slot2"):
+		currentWeapon = 2
 
-func _physics_process(_delta):
-	direction = Vector3()
-	if Input.is_action_pressed("move_forwards"):
-		direction += transform.basis.z
-	if Input.is_action_pressed("move_backwards"):
-		direction -= transform.basis.z
-	if Input.is_action_pressed("move_right"):
-		direction -= transform.basis.x
-	if Input.is_action_pressed("move_left"):
-		direction += transform.basis.x
+	if currentWeapon == 1:
+		gun1.visible = true
+		gun1.shoot()
+	else:
+		gun1.visible = false
+
+func _input(event):
+	#get mouse input for camera rotation
+	if event is InputEventMouseMotion:
+		rotation.y += deg2rad(-event.relative.x * mouse_sense)
+		head.rotation.x += deg2rad(-event.relative.y * mouse_sense)
+		head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
+
+func _process(delta):
+	#camera physics interpolation to reduce physics jitter on high refresh-rate monitors
+	if Engine.get_frames_per_second() > Engine.iterations_per_second:
+		camera.set_as_toplevel(true)
+		camera.global_transform.origin = camera.global_transform.origin.linear_interpolate(head.global_transform.origin, cam_accel * delta)
+		camera.rotation.y = rotation.y
+		camera.rotation.x = head.rotation.x
+	else:
+		camera.set_as_toplevel(false)
+		camera.global_transform = head.global_transform
+		
+func _physics_process(delta):
 	
-	direction = direction.normalized()
+	weapon_select()
 	
-	move_and_slide(direction * speed, Vector3.UP)
+	#get keyboard input
+	direction = Vector3.ZERO
+	var h_rot = global_transform.basis.get_euler().y
+	var f_input = Input.get_action_strength("move_forwards") - Input.get_action_strength("move_backwards")
+	var h_input = Input.get_action_strength("move_left") - Input.get_action_strength("move_right") 
+	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	
+	#jumping and gravity
+	if is_on_floor():
+		snap = -get_floor_normal()
+		accel = ACCEL_DEFAULT
+		gravityVec = Vector3.ZERO
+	else:
+		snap = Vector3.DOWN
+		accel = ACCEL_AIR
+		gravityVec += Vector3.DOWN * gravity * delta
+		
+	if Input.is_action_just_pressed("ui_select") and is_on_floor():
+		snap = Vector3.ZERO
+		gravityVec = Vector3.UP * jump
+	
+	#make it move
+	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
+	movement = velocity + gravityVec
+	
+	move_and_slide_with_snap(movement, snap, Vector3.UP)
